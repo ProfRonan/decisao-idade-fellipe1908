f31134a4149fdb4448b6ba0d5123463abdc364e3
fellipe1908 <128155054+fellipe1908@users.noreply.github.com>
2023-03-29 23:58:30 +0000

    Exercicio Concluido

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 print("Digite sua Idade: ")
 idade = int((input(">")))
-impossivel = False
+impossível = False
 menor = False
 maior = False
 velho = False
 
 if idade < 0:
-    print("impossivel")
+    print("impossível!")
     impossivel = True
 if idade < 18:
-    print("não precisa se alistar")
+    print("não precisa se alistar.")
     menor = True 
 if 18 < idade < 65:
-    print("Não esqueça de votar na proxima eleição")
+    print("Não esqueça de votar na proxima eleição.")
     maior = True
 if idade > 65:
-    print("Pode descansar.")
+    print("Vá descansar.")
     velho = True
-if not(impossivel or menor or maior or velho):
+if not(impossível or menor or maior or velho):
     print("eita!") 
 
 
